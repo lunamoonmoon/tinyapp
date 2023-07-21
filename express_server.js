b2cdea334bfad5f5ca3333e6adcf152905012199
lunamoonmoon <5862lunamoon@gmail.com>
2023-07-21 14:39:27 -0700

    added a response

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require("express"); //creates new instance of express framework
+const app = express(); //executes express function
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
