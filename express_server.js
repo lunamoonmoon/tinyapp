377fd618a7fc229a0d1fd344d5c2ad10b93dbea6
lunamoonmoon <5862lunamoon@gmail.com>
2023-07-21 20:36:56 -0700

    add ejs template

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express"); //creates new instance of express framework
 const app = express(); //executes express function
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
